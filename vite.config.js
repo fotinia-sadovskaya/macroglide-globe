c88f5f5c21d6219798e7cf5c77f1db36cf5add9c
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2025-09-02 22:23:15 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/macrogilde_globe/", // 👈 Це ключовий рядок!
+  base: "/macroglide-globe/", // ← назва репозиторію на GitHub
   plugins: [react()],
-  build: {
-    outDir: "dist" // або "build", якщо твій проєкт використовує цю папку
-  }
 });
 
